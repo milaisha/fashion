ac1f4daba3b1e53f2bfdc75216dea52015abdb6e
Aisha <milaisha098@gmail.com>
2022-04-09 17:53:16 +0300

    add changes on mstr.js

--- mstr.js
+++ mstr.js
@@ -1,18 +1,24 @@
-const hero = document.Queryselector(".hero");
-const slider = document.Queryselector(".slider");
-const logo = document.Queryselector("#logo");
-const hamburger = document.Queryselector(".hamburger");
-const headline= document.Queryselector(".headline");
+document.getElementById("hero").setAttribute("style","width:100px");
+document.getElementById("hero").style.width= '100px';      
+document.getElementById("slider").setAttribute("style","position:absolute;width:100%;height:100vh");
+document.getElementById("slider").style.position= 'absolute';
+document.getElementById("slider").style.width= '100%';
+document.getElementById("slider").style.height= '100vh';
+document.getElementById("#logo").setAttribute("style","opacity:0;top:100%")
+document.getElementById("#logo").style.opacity= '0';
+document.getElementById("#logo").style.top= '100%';
+document.getElementById("hamburger").setAttribute("style","")
+document.getElementById("headline").setAttribute("style","")
 
 
-const tl = new Timelinemax();
-tl.fromTo(hero,1,{height: "0%"},{height: "80%"
+
+      var tl = new Timelinemax({});
+tl.To(hero,1,{height: "0%"},{height: "80%"
 ,ease: Power2. easeinOut})
-.fromTo(hero,1.2{width:"100%"} ,{width:"80%"
+tl.To(hero,1.2,{width:"100%"} ,{width:"80%"
 ,ease: Power2. easeinOut})
-.fromTo(slider,1.2, {x:"-100%"}, {x:"0%",ease: Power2. easeinOut} 
-"-=1.2"
+tl.To(slider,1.2, {x:"-100%"}, {x:"0%",ease: Power2. easeinOut} 
 )
-.fromTo(logo,0.5 {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=0.5")
-.fromTo(hamburger,0.5 {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=0.5")
-.fromTo(headline,0.5 {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=0.5");
+tl.To(logo,0.5,{opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=0.5")
+tl.To(hamburger,0.5 ,{opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=0.5")
+tl.To(headline,0.5 ,{opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=0.5");
