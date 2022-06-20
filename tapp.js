095a2d68a7a0594235e44e7728ea24e621ff032d
Aisha <milaisha098@gmail.com>
2022-06-20 15:41:02 +0300

    add tapp.html

--- tapp.js
+++ tapp.js
@@ -1,2 +1,10 @@
-alert("Salaam!")
-document:getElementById(#add-new-task)
+alert:"kindly fill the form "
+document.getElementById("#new-task-form").innerHTML="form";
+document.getElementById("#new-task-input").innerHTML="add new task";
+document.getElementById("tasks").innerHTML="list_element";
+document.getElementById("input").value="add new-task-input";
+const task_el = document.createElement("div");
+task_el.classList.add("task");
+const task_content_el = document.createElement("div");
+task_content_el.classList.add("content");
+task_content_el.innerHTML=task;
